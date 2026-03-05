9dbf4af1c969c1817e73f67ab74f8860ba688771
Brismi03 <briseidagr22@outlook.com>
2026-03-04 23:39:54 -0600

    Cambios en el form de productos

--- productos/forms.py
+++ productos/forms.py
@@ -4,7 +4,7 @@ from productos.models import Producto
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ['nombre', 'sku', 'descripcion', 'precio', 'unidad_medida', 'stock_minimo', 'activo','categoria']
+        fields = ['nombre', 'sku', 'descripcion', 'precio', 'unidad_medida', 'stock_minimo','categoria']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
             'sku': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sku'}),
@@ -13,10 +13,6 @@ class ProductoForm(forms.ModelForm):
             'unidad_medida': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Ej. pieza, kg, litro'}),
             'stock_minimo': forms.NumberInput(attrs={'class': 'form-control','min': '0'}),
             'categoria': forms.Select(attrs={'class': 'form-select'}),
-            'activo': forms.RadioSelect(choices=[
-                (True, 'Sí'),
-                (False, 'No')
-            ])
         }
         labels = {
             'nombre': 'Nombre del producto',
@@ -26,5 +22,4 @@ class ProductoForm(forms.ModelForm):
             'unidad_medida': 'Unidad de Medida',
             'stok_minimo': 'Stock minímo del producto',
             'categoria': 'Categoria del producto',
-            'activo': '¿El producto esta activo?'
         }
